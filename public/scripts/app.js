3f265ac208750a8b34774a4ff147c6eaf8f29334
adoniramj <adoniramjvargas@gmail.com>
2020-01-15 08:53:44 -0500

    Use Babel to compile JSX to JS
    Use babeljs.io to convert JSX to a React element.
    The React element is rendered in public/index.hmtl.

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+console.log('app.js is running')
+
+//JSX - JavaScript XML
+
+// var template = <p>This is JSX from app.js</p>
+var template = React.createElement("p", null, "This is JSX from app.js");
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
