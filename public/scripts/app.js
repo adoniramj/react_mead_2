f715b6fb89b74f175ffe723b78f3d9f79d8f0406
adoniramj <adoniramjvargas@gmail.com>
2020-01-17 07:39:26 -0500

    Use babel CLI to render JSX. Use if statement inside a function to test JSX rendering. Test the ternary operaton. Test the logical and operator.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,14 +3,81 @@
 console.log('app.js is running');
 
 //JSX - JavaScript XML
-
+var app = {
+  title: 'Indecision  App',
+  subtitle: 'I am a sub-title',
+  options: ['Option one', 'Option two']
+};
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'Testing JSX and Babel'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'You do not have any options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 2'
+    )
+  )
 );
 // var template = React.createElement("p", null, "This is JSX from app.js");
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+var user = {
+  age: 17,
+  location: 'Miami'
+};
+var userName = 'Mike';
+var userAge = 50;
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'p',
+    null,
+    'Name: ',
+    user.name ? user.name : 'anonymous'
+  ),
+  user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
