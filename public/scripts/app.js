eac0d4bdc87e69320bdefad2f10e538e0e1feb6b
adoniramj <adoniramjvargas@gmail.com>
2020-01-17 08:50:31 -0500

    Review ES-5 functions. How does arrow functions bind the this keyword?

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,83 +1,85 @@
 'use strict';
 
-console.log('app.js is running');
-
-//JSX - JavaScript XML
-var app = {
-  title: 'Indecision  App',
-  subtitle: 'I am a sub-title',
-  options: ['Option one', 'Option two']
+var square = function square(x) {
+  return x * x;
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'You do not have any options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item 1'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item 2'
-    )
-  )
-);
-// var template = React.createElement("p", null, "This is JSX from app.js");
+var cube = function cube(x) {
+  return Math.pow(x, 3);
+};
+console.log(square(16));
+console.log(cube(3));
 
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  }
-}
+var squaretwo = function squaretwo(x) {
+  return x * x;
+};
+
+console.log(squaretwo(8));
+
+var name = 'Adoniram Vargas';
+//expression syntax
+var lastName = function lastName(input) {
+  return input.split(' ')[1];
+};
+
+console.log(lastName(name));
+//verbose syntax
+var firstName = function firstName(input) {
+  return input.split(' ')[0];
+};
+
+console.log(firstName(name));
+
+//argument objects are bound in ES5 funcitons.
+
+var add = function add(a, b) {
+  console.log(arguments);
+  return a + b;
+};
+console.log(add(1, 3, 1001));
+
+var addArrow = function addArrow(a, b) {
+  //console.log(arguments)
+  return a + b;
+};
+
+console.log(addArrow(5, 6));
+
 var user = {
-  age: 17,
-  location: 'Miami'
+  name: 'Adoniram',
+  cities: ['Miami', 'Yokosuka'],
+  printPlacesLived: function printPlacesLived() {
+    var _this = this;
+
+    console.log(this.name);
+    console.log(this.cities);
+
+    this.cities.forEach(function (city) {
+      //If you uncomment the line below it will produce an error
+      console.log(this.name + ' has lived in ' + city);
+    }), this.cities.forEach(function (city) {
+      console.log(_this.name + ' has live in ' + city);
+    });
+  },
+  printPlacesLived2: function printPlacesLived2() {
+    undefined.cities.forEach(function (city) {
+      console.log(undefined.name + ' has live in ' + city);
+    });
+  }
 };
-var userName = 'Mike';
-var userAge = 50;
 
-var template2 = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'p',
-    null,
-    'Name: ',
-    user.name ? user.name : 'anonymous'
-  ),
-  user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
-var appRoot = document.getElementById('app');
+user.printPlacesLived();
 
-ReactDOM.render(template, appRoot);
+var multiplier = {
+  numbers: [1, 2, 3, 4, 5],
+  multiplyBy: 5,
+  multiply: function multiply() {
+    var _this2 = this;
+
+    return this.numbers.map(function (number) {
+      return number * _this2.multiplyBy;
+    });
+  }
+};
+
+console.log(multiplier.multiply());
