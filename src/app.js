d5c3e7499112380b206ac354df79511dc35ba247
adoniramj <adoniramjvargas@gmail.com>
2020-01-20 12:09:36 -0500

    Create nested components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render(){
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -24,6 +37,17 @@ class Options extends React.Component {
     return (
       <div>
         Options components here
+        <Option />
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        This is the option component
       </div>
     )
   }
@@ -40,13 +64,5 @@ class AddOption extends React.Component {
 }
 
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-)
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
