d796fd8df49f9b99984c84052b3612be5f302c1d
adoniramj <adoniramjvargas@gmail.com>
2020-01-20 10:23:45 -0500

    Do a review of ES6 class components. Review parent and children class. Review constructor method. Review how to override method

--- src/playground/es6-classes.js
+++ src/playground/es6-classes.js
@@ -1,13 +1,14 @@
 class Person {
+  //This not a function body
   //es6 method definition syntax
-  constructor(name = 'Anonymous', age = 0){
+  constructor(name = 'Anonymous', age = 0) {
     //this refers to the class instance
     this.name = name
     this.age = age
   }
 
   getGreeting() {
-    return `Hi ${this.name}`
+    return `Hi. I am ${this.name}.`
   }
 
   getDescription() {
@@ -15,13 +16,51 @@ class Person {
   }
 }
 
-const me = new Person('Adoniram Vargas', 47)
+class Student extends Person {
+  //tweaking the constructor function
+  constructor(name, age, major) {
+    super(name, age)
+    this.major = major
+  }
 
-console.log(me)
+  hasMajor() {
+    return !!this.major
+  }
 
-const other = new Person()
-console.log(other)
+  getDescription() {
+    let description = super.getDescription()
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}`
+    }
+    return description
+  }
+
+}
+
+class Traveler extends Person {
+  constructor(name, age, location) {
+    super(name, age)
+    this.location = location
+  }
+
+  hasLocation() {
+    return !!this.location
+  }
+  getGreeting() {
+    let greeting = super.getGreeting()
+    if(this.hasLocation()) {
+      greeting += ` And I live in ${this.location}.`
+    }
+    return greeting
+  }
+}
+
+const me = new Traveler('Adoniram Vargas', 47, 'Miami')
 
 console.log(me.getGreeting())
+
+const other = new Traveler('John Doe')
+
 console.log(other.getGreeting())
-console.log(me.getDescription())
+
---
+++ src/playground/indecision-jsx.js
@@ -0,0 +1,66 @@
+console.log('app.js is running')
+
+//JSX - JavaScript XML
+var app = {
+  title: 'Indecision  App',
+  subtitle: 'I am a sub-title',
+  options: ['Option one', 'Option two']
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const option = e.target.elements.option.value
+
+  if (option) {
+    console.log('This option is not empty')
+    app.options.push(option)
+    e.target.elements.option.value = ''
+    reRendeFunc()
+  }
+
+}
+
+const onMakeDecision = () => {
+  const number = Math.floor(Math.random()*app.options.length)
+  const option = app.options[number]
+  console.log(option)
+}
+const clearArray = () => {
+  app.options = []
+  reRendeFunc()
+}
+
+
+var appRoot = document.getElementById('app')
+
+
+const reRendeFunc = () => {
+  var template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'You do not have any options'}</p>
+      <button
+        disabled={app.options.length === 0 ? true : false} 
+        onClick={onMakeDecision}>
+        What should I do?
+      </button>
+      <br />
+      <button onClick={clearArray}>Clear options</button>
+      <ol>
+          {
+            app.options.map((item, index) => {
+              return <li key={index}>{item}</li>
+            })
+          }
+      </ol>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='option' autoComplete='off' />
+        <button>Submit</button>
+      </form>
+    </div>)
+  
+    ReactDOM.render(template, appRoot)
+}
+
+reRendeFunc()
