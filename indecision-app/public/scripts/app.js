4585b0907f91103511e2a0b853508d5cc1169b59
adoniramj <adoniramjvargas@gmail.com>
2020-03-05 17:00:47 -0500

    Review data binding in JSX. JSX does not have build-in data binding capabilities.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,9 +3,8 @@
 console.log("App.js is running!");
 var app = {
   title: 'Indecision App',
-  sutitle: 'Put your life in the hands of a computer.',
-  location: 'New York',
-  age: 15
+  subtitle: 'Put your life in the hands of a computer.',
+  options: ['one', 'two']
 };
 
 function getLocation(location) {
@@ -32,18 +31,12 @@ var template = React.createElement(
     null,
     'Anonymous'
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     'p',
     null,
-    app.sutitle
+    app.subtitle
   ),
-  getLocation(app.location),
-  app.age > 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    app.age
-  ),
+  app.options.length > 0 ? 'Here are your options' : 'There a no options',
   React.createElement(
     'ol',
     null,
@@ -59,5 +52,55 @@ var template = React.createElement(
     )
   )
 );
+
+var count = 0;
+
+var addOne = function addOne() {
+  console.log('add one', count);
+  count++;
+  render();
+};
+var minusOne = function minusOne() {
+  console.log('minus one');
+  count--;
+  render();
+};
+var reset = function reset() {
+  console.log('reset');
+  count = 0;
+  render();
+};
+
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+function render() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+}
+
+render();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,9 +1,8 @@
 console.log("App.js is running!")
 var app = {
   title: 'Indecision App',
-  sutitle: 'Put your life in the hands of a computer.',
-  location: 'New York',
-  age: 15
+  subtitle: 'Put your life in the hands of a computer.',
+  options: ['one', 'two']
 }
 
 function getLocation(location) {
@@ -16,13 +15,45 @@ function getLocation(location) {
 var template = ( 
 <div>
   { app.title ? <h1>{app.title}</h1> : <h1>Anonymous</h1>}
-  <p>{app.sutitle}</p>
-  {getLocation(app.location)}
-  {app.age > 18 && <p>Age: {app.age}</p>}
+  {app.subtitle && <p>{app.subtitle}</p>}
+  {app.options.length > 0 ? 'Here are your options' : 'There a no options'}
   <ol>
     <li>Item 1</li>
     <li>Item 2</li>
   </ol>
 </div>)
+
+let count = 0
+
+const addOne = () => {
+  console.log('add one', count)
+  count++;
+  render()
+}
+const minusOne = () => {
+  console.log('minus one')
+  count--;
+  render()
+}
+const reset = () => {
+  console.log('reset')
+  count = 0
+  render()
+}
+
 var appRoot = document.getElementById("app")
-ReactDOM.render(template, appRoot)
+
+function render () {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+
+  ReactDOM.render(templateTwo, appRoot)
+}
+
+render()
