4da150ae3fdded66a6cb2d1927ced8ffc9bdd19c
adoniramj <adoniramjvargas@gmail.com>
2020-03-09 15:45:30 -0400

    Create a counter app that has state. Create methods that update the counter state.

---
+++ indecision-app/src/counter.js
@@ -0,0 +1,57 @@
+class Counter extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleAddOne = this.handleAddOne.bind(this)
+    this.handleMinusOne = this.handleMinusOne.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+    this.state = {
+      title: 'Counter Application',
+      count: 0
+    }
+  }
+
+  handleAddOne() {
+    this.setState((prevState) => { //the arrow function is called an updater function
+      return { //prevState is the state object before being updated
+        count: prevState.count + 1
+      }
+    })
+
+  }
+
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      }
+    })
+  }
+
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      }
+    })
+  }
+
+  printCount() {
+    console.log(this.state.count)
+  }
+  render () {
+    
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.handleAddOne}>Add one</button>
+        <button onClick={this.handleMinusOne}>Minus one</button>
+        <button onClick={this.handleReset}>Reset counter</button>
+        <button onClick={this.printCount}>Count</button>
+      </div>
+    )
+  }
+}
+
+const appRoot = document.getElementById('app')
+ReactDOM.render(<Counter />, appRoot)
