8d3fdc189d94fdf64e927b4ba21ef1161d5f5508
adoniramj <adoniramjvargas@gmail.com>
2020-03-14 15:42:45 -0400

    Install react-modal. Refactor stateless functional components.

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
-const Option = (props) => {
-  //console.log('The Option component has the following', props)
-  return (
+const Option = (props) => (
     <div>
       <span>{props.optionText}</span>
       <button onClick={() => props.handleDeleteOption(props.optionText)}>Remove</button>
     </div>
   )
-}
 
 export default Option
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -1,29 +1,25 @@
 import React from 'react'
-
 import Option from './Option'
 
-const Options = (props) => {
-  //console.log('The Options component has the following', props)
-  return (
-    <div>
-      <button
-        disabled={props.options.length === 0}
-        onClick={props.handleDeleteAllOptions}
-      >
-        Remove all options
+const Options = (props) => (
+  <div>
+    <button
+      disabled={props.options.length === 0}
+      onClick={props.handleDeleteAllOptions}
+    >
+      Remove all options
       </button>
-      {
-        props.options.map(option => {
-          return <Option
-            key={option}
-            optionText={option}
-            handleDeleteOption={props.handleDeleteOption}
-          />
-        })
-      }
-    </div>
-  )
-}
+    {
+      props.options.map(option => {
+        return <Option
+          key={option}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      })
+    }
+  </div>
+)
 
 export default Options
 
